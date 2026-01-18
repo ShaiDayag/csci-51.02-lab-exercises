dbc76c6337f8c4e35aa9872724aae2b4d2b1249a
ShaiDayag <shaan.dayag@student.ateneo.edu>
2026-01-18 15:43:34 +0800

    feat: add code base for future labs

---
+++ Lab_DagaragaDayagGonzales_code.cpp
@@ -0,0 +1,20 @@
+/*
+Pete Justin Dagaraga (231874)
+Shaan Graal Dayag (231928)
+Robynne Alexa Gonzales (232944)
+
+Month Day, 2026
+*/
+
+/*
+We have not discussed the C++ language code in our program with anyone other than our instructor or the teaching assistants assigned to this course.
+We have not used C++ language code obtained from another student, or any other unauthorized source, either modified or unmodified.
+If any C++ language code or documentation used in our program was obtained from another source, such as a textbook or course notes, that has been clearly noted with a proper citation in the comments of our program.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    return 0;
+}
