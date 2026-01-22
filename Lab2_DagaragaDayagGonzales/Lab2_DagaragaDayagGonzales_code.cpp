59ce11da1141ec76aec03a46a36ccb3c0f2afe57
ShaiDayag <shaan.dayag@student.ateneo.edu>
2026-01-22 23:09:53 +0800

    style: fix blank lines and whitespaces

--- Lab2_DagaragaDayagGonzales/Lab2_DagaragaDayagGonzales_code.cpp
+++ Lab2_DagaragaDayagGonzales/Lab2_DagaragaDayagGonzales_code.cpp
@@ -23,25 +23,40 @@ using namespace std;
  * Returns a spliced string that has the actual message content
  * @param str the string being spliced
  */
-string spaceClearer(string str){ 
-	int strStart; int strEnd; //initializing int variables that denote the starting and ending indices of the actual string content
-	string cleared; //initializing empty string to be returned
-	for(int j = 0 ; j < str.length() ; j++){ //loops through each character starting at the left of the string
-		if (char(str[j]) != ' ') { //if the character at index j is not a whitespace
-			strStart = j; //the value of j is the starting index of the string that has actual content
-			break; //ends the loop as strStart has been found
+string spaceClearer(string str){
+	// initializing int variables that denote the starting and ending indices 
+	// of the actual string content
+	int strStart; int strEnd;
+	// initializing empty string to be returned
+	string cleared;
+	
+	// loops through each character starting at the left of the string
+	for (int j = 0 ; j < str.length() ; j++) {
+		// if the character at index j is not a whitespace
+		if (char(str[j]) != ' ') {
+			// the value of j is the starting index of the string that has actual content
+			strStart = j;
+			break; // ends the loop as strStart has been found
 		}
 	}
-	for(int k = str.length()-1 ; k >= strStart ; k--){ //loops through each character starting at the right of the string, ends at the starting index of the actual string content
-		if (char(str[k]) != ' '){ //if the character at index k is not a whitespace
-			strEnd = k; //the value of k is the ending index of the string that has actual content
-			break; //ends the loop as strEnd has been found
+
+	// loops through each character starting at the right of the string,
+	// ends at the starting index of the actual string content
+	for (int k = str.length()-1 ; k >= strStart ; k--) {
+		// if the character at index k is not a whitespace
+		if (char(str[k]) != ' ') {
+			// the value of k is the ending index of the string that has actual content
+			strEnd = k;
+			break;	// ends the loop as strEnd has been found
 		}
 	}
-	for(int m = strStart ; m <= strEnd ; m++){ //loops through each character found in between (inclusive) locations strStart and strEnd
-		cleared += str[m]; //each character that is of content in str is added to cleared
+	
+	// loops through each character found in between (inclusive) locations strStart and strEnd
+	for (int m = strStart ; m <= strEnd ; m++) {
+		cleared += str[m];	// each character that is of content in str is added to cleared
 	}
-	return cleared; //returns the string with no extra whitespaces before and after it
+	
+	return cleared;	// returns the string with no extra whitespaces before and after it
 }
 
 /**
@@ -50,8 +65,7 @@ string spaceClearer(string str){
  * Used to check if messages with a single character are truly numbers
  * @param c the character being checked
  */
-
-bool isNumberChar(char c){
+bool isNumberChar(char c) {
 	return (int(char(c)) >= 48 && int(char(c)) <= 57); // check if the digit is 0 to 9
 }
 
@@ -64,8 +78,10 @@ bool isNumberChar(char c){
  * the message being a single number message.
  * @param str1 as original message, @param str2 as "translated into integer" message
  */
-
-bool is1LongerThan2(string str1, string str2){ //If str2 somehow gets a value that can easily be an integer, but there is still remaining content unused in the original message, then the message is not of a single integer
+bool is1LongerThan2(string str1, string str2){
+	// If str2 somehow gets a value that can easily be an integer,
+	// but there is still remaining content unused in the original message,
+	// then the message is not of a single integer
 	return str1.length() > str2.length();
 }
 
@@ -75,24 +91,42 @@ bool is1LongerThan2(string str1, string str2){ //If str2 somehow gets a value th
  * Will return "number" if the message is just a single number (not a decimal)
  * @param message as the message whose kind is to be determined
  */
-
-string messageType(string message){ //helps determine if we are yelling or number carding
-	string test; //initialized string that will be used when checking messages that are longer than one character
-	char buffer[message.length()]; //initialized a char array with length of the message's length. To be filled when checking messages that are longer than one character
-	int num = atoi(message.c_str()); //initialized num to hold the integer value of the conversion attempt of message into an integer
-	if(message.length() > 1){ //if message is not a single character
-		snprintf(buffer, message.length()+1, "%d", num); //transcribes the integer into buffer. snprintf always has the last character as a /0, which is why there is a +1, to fully transcribe its contents
-		test += buffer; //buffer now fills the string with its contents
-		if (is1LongerThan2(message,test)){ //checks if message is longer than test
-			return "string"; //if so, then the message was not of a single integer value written in proper order
-		}else{
-			return "number"; //if not, then the message was of a single integer value written in proper order
+string messageType(string message) {	// helps determine if we are yelling or number carding
+	// initialized string that will be used when checking messages that are longer than one character
+	string test;
+	// initialized a char array with length of the message's length. To be filled when checking messages that are longer than one character
+	char buffer[message.length()];
+	// initialized num to hold the integer value of the conversion attempt of message into an integer
+	int num = atoi(message.c_str());
+	
+	// if message is not a single character
+	if (message.length() > 1) {
+		// transcribes the integer into buffer. snprintf always has the last character as a /0, which is why there is a +1, to fully transcribe its contents
+		snprintf(buffer, message.length()+1, "%d", num);
+		// buffer now fills the string with its contents
+		test += buffer;
+		
+		// checks if message is longer than test
+		if (is1LongerThan2(message,test)) {
+			// if so, then the message was not of a single integer value written in proper order
+			return "string";
 		}
-	}else{ //if message is a single character only
-		if (isNumberChar(message[0])){ //checks if the character is a number
-			return "number"; //if so, then the message is an integer
-		}else{
-			return "string"; //if not, then the message is some other non-numerical character
+
+		else {
+			// if not, then the message was of a single integer value written in proper order
+			return "number";
+		}
+	}
+
+	// if message is a single character only
+	else {
+		// checks if the character is a number
+		if (isNumberChar(message[0])) {
+			return "number"; // if so, then the message is an integer
+		}
+		
+		else {
+			return "string"; // if not, then the message is some other non-numerical character
 		}
 	}
 }
@@ -107,22 +141,33 @@ string messageType(string message){ //helps determine if we are yelling or numbe
  * Every 1st line states the agent's coordinates
  * Every 2nd line states the agent's message and how they are delivering it.
 */
+int main(void) {
+	// initializes number of agents, x and y coordinates, and message string
+	int c; int x; int y; string message;
+	
+	cin >> c; // c holds the value signifying number of agents
 
-int main(void){
-	int c; int x; int y; string message; //initializes number of agents, x and y coordinates, and message string
-	cin >> c; //c holds the value signifying number of agents
-	for(int i = 0 ; i < c ; i++){ //loops c amount of times, covering all agents
-		cin >> x >> y; //x and y now hold the coordinates of the ith agent
-		getline(cin, message); //message obtained from the rest of what is left in the line
-		message = spaceClearer(message); //removed all whitespaces before and after message's true content
-		cout << "Agent #" << i+1 << " is at (" << x << ", " << y << ")" << endl; //outputs coordinates of an agent
-		if(messageType(message) == "number"){ //if message is a single number
-			cout << "Agent #" << i+1 << " holds up the number: " << message << endl; //outputs agent holding up a card of that number
-		}else{
-			cout << "Agent #" << i+1 << " yells: " << "\"" << message << "\"" << endl; //outputs agent yelling the message
+	// loops c amount of times, covering all agents
+	for (int i = 0 ; i < c ; i++) {
+		cin >> x >> y; // x and y now hold the coordinates of the ith agent
+		getline(cin, message); // message obtained from the rest of what is left in the line
+		message = spaceClearer(message); // removed all whitespaces before and after message's true content
+		// outputs coordinates of an agent
+		cout << "Agent #" << i+1 << " is at (" << x << ", " << y << ")" << endl;
+
+		// if message is a single number
+		if (messageType(message) == "number") {
+			// outputs agent holding up a card of that number
+			cout << "Agent #" << i+1 << " holds up the number: " << message << endl;
+		}
+
+		else {
+			// outputs agent yelling the message
+			cout << "Agent #" << i+1 << " yells: " << "\"" << message << "\"" << endl;
 		}
 	}
-	return 0; //obligatory, to help determine error if main function fails.
+
+	return 0; // obligatory, to help determine error if main function fails.
 }
 
 /*
