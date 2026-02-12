d23aff1f0184fc255e66c7c6bb0f12b1d9f3a072
bearawr <bearawrbyne@gmail.com>
2026-02-12 13:27:49 +0800

    Completed cpp code

--- Lab4_DagaragaDayagGonzales/Lab4_DagaragaDayagGonzales.cpp
+++ Lab4_DagaragaDayagGonzales/Lab4_DagaragaDayagGonzales.cpp
@@ -1,1 +1,19 @@
+int dummy(int x){
+  return x * 4;
+}
 
+int dummy2(int x){
+  return x * 41;
+}
+
+int dummy3(int x){
+  return x * 63;
+}
+
+int dummy4(int x){
+  return x * -5;
+}
+
+int dummy5(int x){
+  return x * 61;
+}
