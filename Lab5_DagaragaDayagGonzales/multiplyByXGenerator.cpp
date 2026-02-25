cbd7b38244623f3f1022b0f335d397bf511f58d2
PeteJustinDagaraga <pete.dagaraga@student.ateneo.edu>
2026-02-25 17:52:08 +0800

    informational comments for each step significant to our understanding

--- Lab5_DagaragaDayagGonzales/multiplyByXGenerator.cpp
+++ Lab5_DagaragaDayagGonzales/multiplyByXGenerator.cpp
@@ -24,10 +24,10 @@ int main(int argc, char *argv[]){
     cout << "	.cfi_offset 6, -16" << endl;
     cout << "	movq	%rsp, %rbp" << endl;
     cout << "	.cfi_def_cfa_register 6" << endl;
-    cout << "	movq	%rdi, -8(%rbp)" << endl;
-    cout << "	movq	-8(%rbp), %rax" << endl;
-    cout << "	movl	(%rax), %eax" << endl;
-    cout << "	popq	%rbp" << endl;
+    cout << "	movq	%rdi, -8(%rbp)" << endl;                // send IntArray into this function's stackframe address: base pointer - 8
+    cout << "	movq	-8(%rbp), %rax" << endl;                // send IntArray into %rax
+    cout << "	movl	(%rax), %eax" << endl;                  // get size by moving the first 32 bits of %rax into %eax (which is also the last 32 bits of %rax)
+    cout << "	popq	%rbp" << endl;                          // with the register's values changed, end function go back to the point after where it was called
     cout << "	.cfi_def_cfa 7, 8" << endl;
     cout << "	ret" << endl;
     cout << "	.cfi_endproc" << endl;
@@ -47,16 +47,16 @@ int main(int argc, char *argv[]){
     cout << "	.cfi_offset 6, -16" << endl;
     cout << "	movq	%rsp, %rbp" << endl;
     cout << "	.cfi_def_cfa_register 6" << endl;
-    cout << "	movq	%rdi, -8(%rbp)" << endl;
-    cout << "	movl	%esi, -12(%rbp)" << endl;
-    cout << "	movq	-8(%rbp), %rax" << endl;
-    cout << "	movq	8(%rax), %rax" << endl;
-    cout << "	movl	-12(%rbp), %edx" << endl;
-    cout << "	movslq	%edx, %rdx" << endl;
-    cout << "	salq	$2, %rdx" << endl;
-    cout << "	addq	%rdx, %rax" << endl;
-    cout << "	movl	(%rax), %eax" << endl;
-    cout << "	popq	%rbp" << endl;
+    cout << "	movq	%rdi, -8(%rbp)" << endl;                // IntArray sent to address base pointer - 8 
+    cout << "	movl	%esi, -12(%rbp)" << endl;               // current value of i to address base pointer - 12
+    cout << "	movq	-8(%rbp), %rax" << endl;                // IntArray sent to %rax
+    cout << "	movq	8(%rax), %rax" << endl;                 // elements of IntArray sent to %rax
+    cout << "	movl	-12(%rbp), %edx" << endl;               // current value of i sent to %edx
+    cout << "	movslq	%edx, %rdx" << endl;                    // with sign-extend long to quad, move current value of i to %rdx
+    cout << "	salq	$2, %rdx" << endl;                      // i * 4 = %rdx
+    cout << "	addq	%rdx, %rax" << endl;                    // elements of IntArray + i * 4 (this allows us to move along the array) = %rax
+    cout << "	movl	(%rax), %eax" << endl;                  // first 32 bits of %rax is sent to %eax (now holds our element of choosing based on i's value)
+    cout << "	popq	%rbp" << endl;                          // with the register's values changed, end function go back to the point after where it was called
     cout << "	.cfi_def_cfa 7, 8" << endl;
     cout << "	ret" << endl;
     cout << "	.cfi_endproc" << endl;
@@ -76,19 +76,19 @@ int main(int argc, char *argv[]){
     cout << "	.cfi_offset 6, -16" << endl;
     cout << "	movq	%rsp, %rbp" << endl;
     cout << "	.cfi_def_cfa_register 6" << endl;
-    cout << "	movq	%rdi, -8(%rbp)" << endl;
-    cout << "	movl	%esi, -12(%rbp)" << endl;
-    cout << "	movl	%edx, -16(%rbp)" << endl;
-    cout << "	movq	-8(%rbp), %rax" << endl;
-    cout << "	movq	8(%rax), %rax" << endl;
-    cout << "	movl	-12(%rbp), %edx" << endl;
-    cout << "	movslq	%edx, %rdx" << endl;
-    cout << "	salq	$2, %rdx" << endl;
-    cout << "	addq	%rax, %rdx" << endl;
-    cout << "	movl	-16(%rbp), %eax" << endl;
-    cout << "	movl	%eax, (%rdx)" << endl;
-    cout << "	nop" << endl;
-    cout << "	popq	%rbp" << endl;
+    cout << "	movq	%rdi, -8(%rbp)" << endl;                // IntArray to address base pointer - 8
+    cout << "	movl	%esi, -12(%rbp)" << endl;               // current value of i to address base pointer - 12
+    cout << "	movl	%edx, -16(%rbp)" << endl;               // the product calculated to address base pointer - 16
+    cout << "	movq	-8(%rbp), %rax" << endl;                // IntArray sent to %rax
+    cout << "	movq	8(%rax), %rax" << endl;                 // elements of IntArray sent to %rax
+    cout << "	movl	-12(%rbp), %edx" << endl;               // current value of i sent to %edx
+    cout << "	movslq	%edx, %rdx" << endl;                    // with sign-extend long to quad, move current value of i to %rdx
+    cout << "	salq	$2, %rdx" << endl;                      // i * 4 = %rdx
+    cout << "	addq	%rax, %rdx" << endl;                    // i * 4 + elements of IntArray (this allows us to move along the array) = %rdx
+    cout << "	movl	-16(%rbp), %eax" << endl;               // the product calculated is moved into %eax
+    cout << "	movl	%eax, (%rdx)" << endl;                  // replace the element found at elements[i] with the product
+    cout << "	nop" << endl;                                   // idle
+    cout << "	popq	%rbp" << endl;                          // with the register's values changed, end function go back to the point after where it was called
     cout << "	.cfi_def_cfa 7, 8" << endl;
     cout << "	ret" << endl;
     cout << "	.cfi_endproc" << endl;
@@ -110,34 +110,34 @@ int main(int argc, char *argv[]){
     cout << "	pushq	%rbx" << endl;
     cout << "	subq	$40, %rsp" << endl;
     cout << "	.cfi_offset 3, -24" << endl;
-    cout << "	movq	%rdi, -40(%rbp)" << endl;
-    cout << "	movl	$0, -20(%rbp)" << endl;
-    cout << "	jmp	.L7" << endl;
+    cout << "	movq	%rdi, -40(%rbp)" << endl;               // send the IntArray into the stackframe address base pointer - 40
+    cout << "	movl	$0, -20(%rbp)" << endl;                 // initialize i as 0, with stackframe address as base pointer - 20
+    cout << "	jmp	.L7" << endl;                               // jump to L7 (always)
     cout << ".L8:" << endl;
-    cout << "	movq	-40(%rbp), %rbx" << endl;
-    cout << "	movl	-20(%rbp), %edx" << endl;
-    cout << "	movq	-40(%rbp), %rax" << endl;
-    cout << "	movl	%edx, %esi" << endl;
-    cout << "	movq	%rax, %rdi" << endl;
-    cout << "	call	_ZN8IntArray10getElementEi" << endl;
+    cout << "	movq	-40(%rbp), %rbx" << endl;               // send IntArray to %rbx
+    cout << "	movl	-20(%rbp), %edx" << endl;               // send current value of i to %edx (which is the last 32 bits of %rdx)
+    cout << "	movq	-40(%rbp), %rax" << endl;               // send IntArray to %rax
+    cout << "	movl	%edx, %esi" << endl;                    // move current value of i to %esi
+    cout << "	movq	%rax, %rdi" << endl;                    // send IntArray to %rdi
+    cout << "	call	_ZN8IntArray10getElementEi" << endl;    // refer to getElement function call
     //This area is most likely going to need to change for the sake of optimization
-    cout << "	imull	$" << x << ", %eax, %edx" << endl;
-    cout << "	movl	-20(%rbp), %eax" << endl;
-    cout << "	movl	%eax, %esi" << endl;
-    cout << "	movq	%rbx, %rdi" << endl;
-    cout << "	call	_ZN8IntArray10setElementEii" << endl;
-    cout << "	addl	$1, -20(%rbp)" << endl;
+    cout << "	imull	$" << x << ", %eax, %edx" << endl;      // chosen element (currently in %eax) multiplied by x, stored in %edx [this part is what we need to change]
+    cout << "	movl	-20(%rbp), %eax" << endl;               // send current value of i to %eax
+    cout << "	movl	%eax, %esi" << endl;                    // send current value of i to %esi
+    cout << "	movq	%rbx, %rdi" << endl;                    // send IntArray to %rdi
+    cout << "	call	_ZN8IntArray10setElementEii" << endl;   // refer to setElement function call
+    cout << "	addl	$1, -20(%rbp)" << endl;                 // i++
     cout << ".L7:" << endl;
-    cout << "	movq	-40(%rbp), %rax" << endl;
-    cout << "	movq	%rax, %rdi" << endl;
-    cout << "	call	_ZN8IntArray7getSizeEv" << endl;
-    cout << "	cmpl	%eax, -20(%rbp)" << endl;
-    cout << "	setl	%al" << endl;
-    cout << "	testb	%al, %al" << endl;
-    cout << "	jne	.L8" << endl;
-    cout << "	nop" << endl;
-    cout << "	nop" << endl;
-    cout << "	movq	-8(%rbp), %rbx" << endl;
+    cout << "	movq	-40(%rbp), %rax" << endl;               // move the IntArray into %rax
+    cout << "	movq	%rax, %rdi" << endl;                    // move the IntArray into $rdi
+    cout << "	call	_ZN8IntArray7getSizeEv" << endl;        // refer to getSize function call
+    cout << "	cmpl	%eax, -20(%rbp)" << endl;               // with %eax (lower 32 bits of %rax) containing the size of IntArray, the value of i and size is compared
+    cout << "	setl	%al" << endl;                           // results of the comparison are stored in %al
+    cout << "	testb	%al, %al" << endl;                      // Confirms if %al is 1 or 0 via %al AND %al
+    cout << "	jne	.L8" << endl;                               // If %al is not equal to 0, then %al is 1 and that means i < size, which leads us to jump to L8. Otherwise, this function is basically done
+    cout << "	nop" << endl;                                   // idle
+    cout << "	nop" << endl;                                   // idle
+    cout << "	movq	-8(%rbp), %rbx" << endl;                // ???
     cout << "	leave" << endl;
     cout << "	.cfi_def_cfa 7, 8" << endl;
     cout << "	ret" << endl;
