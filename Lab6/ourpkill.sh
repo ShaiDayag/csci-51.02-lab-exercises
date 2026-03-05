96b8a7bba74ecf2aa63c825591a01fa47e5f614f
bearawr <bearawrbyne@gmail.com>
2026-03-05 16:10:25 +0800

    edited quiet processes

--- Lab6/ourpkill.sh
+++ Lab6/ourpkill.sh
@@ -14,46 +14,34 @@
 # _______________________________________________________________________________________________
 
 #!/bin/bash
-# Use ourpgrep.sh to find the PID then terminate them.
 
-if [ -z $1 ]
+if [ -z "$*" ]
 then
     echo "Error: Provide process name - ./ourpgrep.sh [pattern]"
     exit 1
 fi
 
-# Determine if $1 is a signal or a name pattern
+# check for the signal
 first_char=$(echo $1 | cut -c1)
 
 if [ "$first_char" = "-" ]
 then
-    # User provided a signal (like -9)
     signal=$1
     pattern=$2
 else
-    # No signal provided, use default -15 (SIGTERM)
+    # default signal
     signal="-15"
     pattern=$1
 fi
 
-# If a signal was given, make sure a pattern followed it
-if [ -z $pattern ]
-then
-    echo Error: No process name pattern provided after signal.
-    exit 1
-fi
+# call ourpgrep.sh and redirect to a temp file (Slide 8)
+./ourpgrep.sh $pattern > .tmp_pids
 
-# Get the PIDs using ourpgrep.sh and Redirect to a temp file
-./ourpgrep.sh $pattern > temp_pids.txt
-
-# Use a for loop to kill each process ID
-# We use $(cat ...) to turn the file content into a list.
-for id in $(cat temp_pids.txt)
+# loop through the PIDs
+for id in $(cat .tmp_pids)
 do
-    # Call the kill command
-    # Redirecting error to /dev/null ensures it stays "quiet"
-    kill $signal $id 2> /dev/null
+    # redirecting to /dev/null makes it output nothing
+    kill $signal $id > /dev/null 2>&1
 done
 
-# Delete the temp file
-rm temp_pids.txt
+rm .tmp_pids
