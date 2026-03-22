945c35b71d4579d694b5ba4bb449ca77a0d0c782
PeteJustinDagaraga <pete.dagaraga@student.ateneo.edu>
2026-03-22 13:48:57 +0800

    Every 3 seconds, print date. Every 3 prints, warn to close

--- Lab7_DagaragaDayagGonzales/mytimer.cpp
+++ Lab7_DagaragaDayagGonzales/mytimer.cpp
@@ -2,6 +2,9 @@
 #include <unistd.h>
 #include <fstream>
 #include <string>
+#include <ctime>
+#include <chrono>
+#include <thread>
 using namespace std;
 
 int main(int argc, char* argv[]){
@@ -12,15 +15,14 @@ int main(int argc, char* argv[]){
 		system("whereis xclock | cut -d' ' -f2 >> xclock_path.txt");
 		ifstream file("xclock_path.txt");
 		string line;
-		const char* path;
 		if( file.is_open() ){
 			getline(file, line);
 			file.close();
 			system("rm xclock_path.txt");
-			path = line.c_str();
 		}else{
 			cerr << "Can\'t open file!" << endl;
 		}
+		const char* path = line.c_str();
 		if( execl(path, "myXclock", NULL) == -1){
 			cout << "It didn't work" << endl;
 		}
@@ -30,11 +32,24 @@ int main(int argc, char* argv[]){
 		string waitEnter;
 		getline(cin, waitEnter);
 		cout << "\"Terminated\"" << endl;
+		
 	} else {
 		// Parent PID
 		// For Parent (NOT COMPLETE)
 		cout << "A loop should be here " << parentID << endl;
+		int counter = 0;
 		do{
+			time_t timestamp = time(NULL);
+			struct tm datetime = *localtime(&timestamp);
+			char dateoutput[22];
+			strftime(dateoutput, 22, "[%F] %T", &datetime);
+			cout << dateoutput << endl;
+			counter++;
+			if(counter == 3){
+				cout << "\"This program has gone on for far too long. Close the myXclock window or press Enter on this window to exit.\"" << endl;
+				counter = 0;
+			}
+			this_thread::sleep_for(chrono::seconds(3));
 		}while(true);
 	}
 }
